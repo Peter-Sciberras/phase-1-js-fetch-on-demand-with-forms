c3e1dafe750d87e5eb3048726503ad108f264e5a
Peter-Sciberras <sciberrasp@yahoo.com.au>
2023-02-16 11:33:44 +1100

    done with assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector('form');
+  const movieDetails = document.getElementById('movieDetails');
 
-document.addEventListener('DOMContentLoaded', init);
+  inputForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const input = event.target.children[1].value;
+    fetch(`/movies/${input}`)
+      .then(response => response.json())
+      .then(data => {
+        movieDetails.children[0].textContent = data.title;
+        movieDetails.children[1].textContent = data.summary;
+      })
+      .catch(error => {
+        movieDetails.children[0].textContent = "Movie not found";
+        movieDetails.children[1].textContent = "";
+      });
+  });
+}
+  document.addEventListener('DOMContentLoaded', init);
